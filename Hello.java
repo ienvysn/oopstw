3f61accce62b723d3aec4b01f38438090257b686
Lavnik <ienvysnn@gmail.com>
2025-04-10 10:23:59 +0545

    First commit

---
+++ Hello.java
@@ -0,0 +1,43 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println( "hello world\n This is my first module \n I am on module ST4003CEM"  );
+        System.out.println("-----------------------------------");
+        System.out.println("-----------------------------------");
+
+        System.out.println("A \"quoted\" String is \n");
+        System.out.println("\'much\' better if you learn \n");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also -, \" \" represents an empty string");
+        System.out.println("String. Don't forget: use \\\"");
+        System.out.println("instead of \" !");
+        System.out.println("'' is not the same as \"");
+
+        System.out.println("-----------------------------------");
+        System.out.println("-----------------------------------");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        System.out.println("-----------------------------------");
+        System.out.println("-----------------------------------");
+
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+
+        System.out.println("-----------------------------------");
+        System.out.println("-----------------------------------");
+
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("What is the difference between");
+        System.out.println("a \' and a \" ? or between  a\" and a\"");
+    }
+}
+
