a541d19555ccb9d54e74fcfae429d97dcd8d82b2
Lavnik <ienvysnn@gmail.com>
2025-04-10 11:37:52 +0545

    addede

--- test.java
+++ test.java
@@ -1,14 +1,4 @@
 public class test {
-    
-}
-
-
-basd
-asasf
-public class test {
-    asdasd
-}
-public class test {
     System.out.println("test hieeeeeeeeeeeee");
     System.out.println("HIEEEEEEEEEEE");
     
